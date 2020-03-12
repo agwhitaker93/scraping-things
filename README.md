0d02719452ec3cdef384d9ab6285397619f4dc20
Andrew Whitaker <agwhitaker93@gmail.com>
2020-03-12 19:53:54 +0000

    implement scraping for hacker news

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Just a little program to pull a list of youtube links from a page
+Just a little program to pull a list of links from a page
--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -5,38 +5,56 @@
             [clojure.java.io :as io])
   (:gen-class))
 
+(defn print-n-return [thing-to-print]
+  (println thing-to-print "\n")
+  thing-to-print)
+
 (defn get-page-body [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn get-nested-page-data [page-data]
-  (html/select page-data [:blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :p :a]))
+(defn get-nested-page-data [nesting page-data]
+  (html/select page-data nesting))
 
 (defn get-elem-attr-content
   [urls]
-  (map (fn [elem]
-         (get-in elem [:attrs :content]))
+  (map #(get-in %1 [:attrs :content])
        urls))
 
-(defn get-elem-with-redirect
+(defn follow-link-for-redirect
   [url]
-  (as-> url $
-    (get-page-body $)
-    (html/select $ [:meta])
-    (filter #(contains? (:attrs %1) :http-equiv) $)))
+    (as-> url $
+      (get-page-body $)
+      (html/select $ [:meta])
+      (filter #(contains? (:attrs %1) :http-equiv) $)
+      (filter #(= "redirect" (get-in %1 [:attrs :http-equiv])) $)))
 
-(defn get-song-urls-from-page
-  [url ]
+(defn filter-valid-links [page-data]
+  (filter #(as-> %1 $
+             (get-in $ [:attrs :href])
+             (re-seq #"^https?:\/\/" $)
+             (empty? $)
+             (not $))
+          page-data))
+
+(defn get-urls-from-page
+  [url nesting]
   (->> url
-      (get-page-body)
-      (get-nested-page-data)
-      (reduce #(merge %1 {(html/text %2)
-                          (-> %2
-                              (get-in [:attrs :href])
-                              (get-elem-with-redirect)
-                              (get-elem-attr-content)
-                              (first)
-                              (string/replace #"0;URL=" ""))})
-              {})))
+       (get-page-body)
+       (get-nested-page-data nesting)
+       (filter-valid-links)
+       (reduce #(merge %1 {(html/text %2)
+                           (as-> %2 $
+                             (get-in $ [:attrs :href])
+                             (try (let [redirect-elems (follow-link-for-redirect $)]
+                                    (if (empty? redirect-elems)
+                                      $
+                                      (-> redirect-elems
+                                          (print-n-return)
+                                          (get-elem-attr-content)
+                                          (first)
+                                          (string/replace #"0;URL=" ""))))
+                                  (catch Exception e (.getMessage e))))})
+               {})))
 
 (defn write-csv
   [filename contents]
@@ -44,8 +62,15 @@
     (csv/write-csv writer
                    contents)))
 
+(defn ambient-sounds []
+  (write-csv "ambient-sounds.csv"
+             (get-urls-from-page "https://nyriad.tumblr.com/post/176288223047/ambient-sounds-for-writers" [:blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :blockquote :p :a])))
+
+(defn hn []
+  (write-csv "hn.csv"
+             (get-urls-from-page "https://news.ycombinator.com/" [:td.title :a])))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (write-csv "atmospheric-music.csv" 
-             (get-song-urls-from-page "https://nyriad.tumblr.com/post/176288223047/ambient-sounds-for-writers")))
+  (ambient-sounds)
+  (hn))
